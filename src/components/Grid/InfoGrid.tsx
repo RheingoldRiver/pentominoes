e7d660d25887f2b0ff8212e569d72037be5a0db1
RheingoldRiver <18037011+RheingoldRiver@users.noreply.github.com>
2023-10-29 07:33:30 -0500

    Fix information board reset confirmations

--- src/components/Grid/InfoGrid.tsx
+++ src/components/Grid/InfoGrid.tsx
@@ -4,23 +4,24 @@ import { ReactNode, memo, useContext } from "react";
 import { GameStateContext } from "../GameStateProvider/GameStateProvider";
 import { PENTOMINOES } from "../../pentominoes";
 
-export const InfoGrid = memo(({ grid, children }: { grid: PlacedPentomino[][]; children: ReactNode }) => {
-  const { setGrid } = useContext(GameStateContext);
+export const InfoGrid = memo(({ grid: displayGrid, children }: { grid: PlacedPentomino[][]; children: ReactNode }) => {
+  const { grid, setGrid } = useContext(GameStateContext);
   return (
     <div
       className={clsx("grid grid-flow-row w-fit h-fit cursor-pointer")}
       style={{
-        gridTemplateRows: `repeat(${grid.length}, minmax(0, 1fr))`,
-        gridTemplateColumns: `repeat(${grid[0].length}, minmax(0, 1fr))`,
+        gridTemplateRows: `repeat(${displayGrid.length}, minmax(0, 1fr))`,
+        gridTemplateColumns: `repeat(${displayGrid[0].length}, minmax(0, 1fr))`,
       }}
       onClick={() => {
         let gridIsEmpty = true;
+        // check against the grid from game state
         grid.forEach((row) =>
           row.forEach((cell) => {
             if (cell.pentomino.name !== PENTOMINOES.None.name) gridIsEmpty = false;
           })
         );
-        if (gridIsEmpty || confirm("Reset your board? You won't be able to undo.")) setGrid(grid);
+        if (gridIsEmpty || confirm("Reset your board? You won't be able to undo.")) setGrid(displayGrid);
       }}
     >
       {children}
