5d8ad4514325dc9d3ea234c1a75eaa6b4c11748d
RheingoldRiver <18037011+RheingoldRiver@users.noreply.github.com>
2023-11-01 20:24:17 -0500

    some dsa absolute bullshit

--- src/components/BottomToolbar/bottomToolbar.utils.test.ts
+++ src/components/BottomToolbar/bottomToolbar.utils.test.ts
@@ -1,78 +1,20 @@
 import { expect, test } from "vitest";
 import { EMPTY_GRID } from "../../constants";
 import { PENTOMINOES } from "../../pentominoes";
-import {
-  cellIsBlocked,
-  cornerIsValid,
-  cornerIsValidSquare,
-  getWrap,
-  getXBlockPos,
-  getXPos,
-  getYBlockPos,
-  getYPos,
-  invalidSolve,
-} from "./bottomToolbar.utils";
-
-test("wrapping is correct", () => {
-  const grid = EMPTY_GRID(8, 8);
-  expect(getWrap(0, 0, grid, "x")).toBe(0);
-  expect(getWrap(0, 1, grid, "x")).toBe(1);
-  expect(getWrap(7, 0, grid, "x")).toBe(7);
-  expect(getWrap(7, 1, grid, "x")).toBe(6);
-});
-
-test("getting x position is correct", () => {
-  const grid = EMPTY_GRID(8, 8);
-  expect(getXPos(0, 0, grid, "x")).toBe(0);
-  expect(getYPos(0, 0, grid, "x")).toBe(1);
-  expect(getXPos(0, 1, grid, "x")).toBe(1);
-  expect(getYPos(0, 1, grid, "x")).toBe(1);
-  expect(getXPos(0, 2, grid, "x")).toBe(2);
-  expect(getYPos(0, 2, grid, "x")).toBe(1);
-
-  expect(getXPos(0, 0, grid, "y")).toBe(1);
-  expect(getYPos(0, 0, grid, "y")).toBe(0);
-  expect(getXPos(0, 1, grid, "y")).toBe(1);
-  expect(getYPos(0, 1, grid, "y")).toBe(1);
-  expect(getXPos(0, 2, grid, "y")).toBe(1);
-  expect(getYPos(0, 2, grid, "y")).toBe(2);
-});
-
-test("getting x position is correct on far end of axis", () => {
-  const grid = EMPTY_GRID(8, 8);
-  expect(getXPos(7, 0, grid, "x")).toBe(7);
-  expect(getYPos(7, 0, grid, "x")).toBe(6);
-  expect(getXPos(7, 1, grid, "x")).toBe(6);
-  expect(getYPos(7, 1, grid, "x")).toBe(6);
-  expect(getXPos(7, 2, grid, "x")).toBe(5);
-  expect(getYPos(7, 2, grid, "x")).toBe(6);
-
-  expect(getXPos(7, 0, grid, "y")).toBe(6);
-  expect(getYPos(7, 0, grid, "y")).toBe(7);
-  expect(getXPos(7, 1, grid, "y")).toBe(6);
-  expect(getYPos(7, 1, grid, "y")).toBe(6);
-  expect(getXPos(7, 2, grid, "y")).toBe(6);
-  expect(getYPos(7, 2, grid, "y")).toBe(5);
-});
-
-test("finding the block position is correct", () => {
-  const grid = EMPTY_GRID(8, 8);
-  expect(getXBlockPos(0, 0, grid, "x")).toBe(1);
-  expect(getYBlockPos(0, 0, grid, "x")).toBe(0);
-  expect(getXBlockPos(0, 2, grid, "x")).toBe(3);
-  expect(getYBlockPos(0, 2, grid, "x")).toBe(0);
-
-  expect(getXBlockPos(0, 0, grid, "y")).toBe(0);
-  expect(getYBlockPos(0, 0, grid, "y")).toBe(1);
-  expect(getXBlockPos(0, 2, grid, "y")).toBe(0);
-  expect(getYBlockPos(0, 2, grid, "y")).toBe(3);
-});
+import { getAdjacentArea, invalidSolve } from "./bottomToolbar.utils";
 
 test("negative control", () => {
   const grid = EMPTY_GRID(8, 8);
-  expect(cellIsBlocked(grid, 0, 0)).toBe(false);
-  expect(cellIsBlocked(grid, 1, 1)).toBe(false);
-  expect(cellIsBlocked(grid, 7, 7)).toBe(false);
+  expect(getAdjacentArea(grid)).toBe(64);
+});
+
+test("works with some random tiles", () => {
+  const grid = EMPTY_GRID(8, 8);
+  grid[0][1].pentomino = PENTOMINOES.R;
+  grid[5][6].pentomino = PENTOMINOES.R;
+  grid[2][3].pentomino = PENTOMINOES.R;
+  grid[7][7].pentomino = PENTOMINOES.R;
+  expect(getAdjacentArea(grid)).toBe(60);
   expect(invalidSolve(grid)).toBe(false);
 });
 
@@ -80,8 +22,6 @@ test("validating a corner properly: trivial case", () => {
   const grid = EMPTY_GRID(8, 8);
   grid[0][1].pentomino = PENTOMINOES.R;
   grid[1][0].pentomino = PENTOMINOES.R;
-  expect(cornerIsValid(grid, "x", { x: 0, y: 0 })).toBe(false);
-  expect(cornerIsValid(grid, "y", { x: 0, y: 0 })).toBe(false);
   expect(invalidSolve(grid)).toBe(true);
 });
 
@@ -89,8 +29,6 @@ test("validating a corner properly: trivial case", () => {
   const grid = EMPTY_GRID(8, 8);
   grid[6][0].pentomino = PENTOMINOES.R;
   grid[7][1].pentomino = PENTOMINOES.R;
-  expect(cornerIsValid(grid, "x", { x: 7, y: 0 })).toBe(false);
-  expect(cornerIsValid(grid, "y", { x: 7, y: 0 })).toBe(false);
   expect(invalidSolve(grid)).toBe(true);
 });
 
@@ -99,8 +37,6 @@ test("validating a corner properly: rectangle case", () => {
   grid[0][1].pentomino = PENTOMINOES.R;
   grid[1][1].pentomino = PENTOMINOES.R;
   grid[2][0].pentomino = PENTOMINOES.R;
-  expect(cornerIsValid(grid, "x", { x: 0, y: 0 })).toBe(false);
-  expect(cornerIsValid(grid, "y", { x: 0, y: 0 })).toBe(true);
   expect(invalidSolve(grid)).toBe(true);
 });
 
@@ -110,8 +46,6 @@ test("validating a corner properly: rectangle case 2", () => {
   grid[1][1].pentomino = PENTOMINOES.R;
   grid[1][2].pentomino = PENTOMINOES.R;
   grid[0][3].pentomino = PENTOMINOES.R;
-  expect(cornerIsValid(grid, "x", { x: 0, y: 0 })).toBe(true);
-  expect(cornerIsValid(grid, "y", { x: 0, y: 0 })).toBe(false);
   expect(invalidSolve(grid)).toBe(true);
 });
 
@@ -136,10 +70,6 @@ test("validating a center properly", () => {
   grid[4][3].pentomino = PENTOMINOES.R;
   grid[3][4].pentomino = PENTOMINOES.R;
   grid[5][4].pentomino = PENTOMINOES.R;
-  expect(cellIsBlocked(grid, 4, 4)).toBe(true);
-  expect(cellIsBlocked(grid, 5, 5)).toBe(false);
-  expect(cellIsBlocked(grid, 4, 5)).toBe(false);
-  expect(cellIsBlocked(grid, 1, 1)).toBe(false);
   expect(invalidSolve(grid)).toBe(true);
 });
 
@@ -148,6 +78,7 @@ test("validating a center properly", () => {
   grid[0][3].pentomino = PENTOMINOES.R;
   grid[0][1].pentomino = PENTOMINOES.R;
   grid[1][2].pentomino = PENTOMINOES.R;
+  grid[7][7].pentomino = PENTOMINOES.R;
   expect(invalidSolve(grid)).toBe(true);
 });
 
@@ -157,7 +88,6 @@ test("validating a corner 2x2 square", () => {
   grid[1][2].pentomino = PENTOMINOES.R;
   grid[2][0].pentomino = PENTOMINOES.R;
   grid[2][1].pentomino = PENTOMINOES.R;
-  expect(cornerIsValidSquare(grid, { x: 0, y: 0 })).toBe(false);
   expect(invalidSolve(grid)).toBe(true);
 
   const grid2 = EMPTY_GRID(8, 8);
@@ -165,6 +95,5 @@ test("validating a corner 2x2 square", () => {
   grid2[1][5].pentomino = PENTOMINOES.R;
   grid2[2][7].pentomino = PENTOMINOES.R;
   grid2[2][6].pentomino = PENTOMINOES.R;
-  expect(cornerIsValidSquare(grid2, { x: 0, y: 7 })).toBe(false);
   expect(invalidSolve(grid2)).toBe(true);
 });
--- src/components/BottomToolbar/bottomToolbar.utils.ts
+++ src/components/BottomToolbar/bottomToolbar.utils.ts
@@ -1,4 +1,4 @@
-import { range } from "lodash";
+import { cloneDeep } from "lodash";
 import { Dimensions, PlacedPentomino } from "../../constants";
 import { Coordinates, PENTOMINOES } from "../../pentominoes";
 
@@ -30,148 +30,45 @@ export const RANDOM_TERRAIN_ALLOWED: Dimensions = {
   width: 8,
 };
 
-export function invalidSolve(grid: PlacedPentomino[][]) {
-  let invalid = false;
-  grid.forEach((row, x) =>
-    row.forEach((c, y) => {
-      // a cell which is itself terrain can't be the center of an invalid cluster
-      if (c.pentomino.name !== PENTOMINOES.None.name) return;
-      if (cellIsBlocked(grid, x, y)) invalid = true;
-    })
-  );
-  if (invalid) return true;
-  const corners = [
-    { x: 0, y: 0 },
-    { x: grid.length - 1, y: 0 },
-    { x: 0, y: grid[0].length - 1 },
-    { x: grid.length - 1, y: grid[0].length - 1 },
-  ];
-  corners.forEach((coords) => {
-    ["x", "y"].forEach((dim) => {
-      if (!cornerIsValid(grid, dim as keyof Coordinates, coords)) invalid = true;
-    });
-    if (!cornerIsValidDiagonal(grid, coords)) invalid = true;
-    if (!cornerIsValidSquare(grid, coords)) invalid = true;
-  });
-  return invalid;
-}
-
-const middleCellInvalidState: Coordinates[] = [
+const adjacentCells: Coordinates[] = [
   { x: -1, y: 0 },
   { x: 1, y: 0 },
   { x: 0, y: 1 },
   { x: 0, y: -1 },
 ];
 
-const maxRangeToCheck = 3;
-
-export function cellIsBlocked(grid: PlacedPentomino[][], x: number, y: number) {
-  let isBlocked = true;
-  middleCellInvalidState.forEach((coords) => {
-    const curX = coords.x + x;
-    const curY = coords.y + y;
-    if (!(0 <= curX && curX < grid.length)) return;
-    if (!(0 <= curY && curY < grid[0].length)) return;
-    if (grid[curX][curY].pentomino.name === PENTOMINOES.None.name) isBlocked = false;
-  });
-  return isBlocked;
+export function invalidSolve(gridToCheck: PlacedPentomino[][]) {
+  const area = getAdjacentArea(gridToCheck);
+  return area % 5 !== 0;
 }
 
-export function getWrap(dimValue: number, offset: number, grid: PlacedPentomino[][], dim: keyof Coordinates) {
-  if (dimValue === 0) return offset;
-  return (dim === "x" ? grid.length : grid[0].length) - 1 - offset;
-}
-
-export function getXPos(dimValue: number, offset: number, grid: PlacedPentomino[][], dim: keyof Coordinates) {
-  if (dim === "x") return getWrap(dimValue, offset, grid, dim);
-  return getWrap(dimValue, 1, grid, dim);
-}
-
-export function getYPos(dimValue: number, offset: number, grid: PlacedPentomino[][], dim: keyof Coordinates) {
-  if (dim === "y") return getWrap(dimValue, offset, grid, dim);
-  return getWrap(dimValue, 1, grid, dim);
-}
-
-export function getXBlockPos(dimValue: number, offset: number, grid: PlacedPentomino[][], dim: keyof Coordinates) {
-  if (dim === "y") return dimValue;
-  return getWrap(dimValue, offset + 1, grid, dim);
-}
-
-export function getYBlockPos(dimValue: number, offset: number, grid: PlacedPentomino[][], dim: keyof Coordinates) {
-  if (dim === "x") return dimValue;
-  return getWrap(dimValue, offset + 1, grid, dim);
-}
-
-export function cornerIsValid(grid: PlacedPentomino[][], dim: keyof Coordinates, startingCoords: Coordinates) {
-  let valid = false;
-  let foundBlock = false;
-
-  const { x, y } = startingCoords;
-
-  const xPos = (dimValue: number, offset: number) => {
-    return getXPos(dimValue, offset, grid, dim);
-  };
-
-  const yPos = (dimValue: number, offset: number) => {
-    return getYPos(dimValue, offset, grid, dim);
-  };
-
-  const xBlockPos = (dimValue: number, offset: number) => {
-    return getXBlockPos(dimValue, offset, grid, dim);
-  };
-
-  const yBlockPos = (dimValue: number, offset: number) => {
-    return getYBlockPos(dimValue, offset, grid, dim);
-  };
-
-  if (grid[x][y].pentomino.name !== PENTOMINOES.None.name) return true;
-  range(maxRangeToCheck).forEach((i) => {
-    if (grid[xPos(x, i)][yPos(y, i)].pentomino.name === PENTOMINOES.None.name) {
-      valid = !foundBlock;
-      return;
-    }
-    if (grid[xBlockPos(x, i)][yBlockPos(y, i)].pentomino.name !== PENTOMINOES.None.name) {
-      foundBlock = true;
-      return;
-    }
-  });
-  if (valid) return true;
-  return !foundBlock;
-}
-
-export function cornerIsValidDiagonal(grid: PlacedPentomino[][], startingCoords: Coordinates) {
-  const valid = [
-    {
-      len: 3,
-      result: false,
-    },
-    {
-      len: 4,
-      result: false,
-    },
-  ];
-  const { x, y } = startingCoords;
-  valid.forEach((val) => {
-    range(val.len).forEach((i) => {
-      const cell = grid[getWrap(x, val.len - 1 - i, grid, "x")][getWrap(y, i, grid, "y")];
-      if (cell.pentomino.name === PENTOMINOES.None.name) {
-        val.result = true;
-      }
+export function getAdjacentArea(gridToCheck: PlacedPentomino[][]) {
+  const grid = cloneDeep(gridToCheck);
+  const tileQueue: PlacedPentomino[] = [];
+  let x = 0;
+  let firstCell: PlacedPentomino;
+  do {
+    firstCell = grid[x][0];
+    x++;
+  } while (firstCell.pentomino.name !== PENTOMINOES.None.name);
+  tileQueue.push(firstCell);
+  let area = 0;
+  firstCell.pentomino = PENTOMINOES.Found;
+  // eslint-disable-next-line no-constant-condition
+  while (true) {
+    const curCell = tileQueue.pop();
+    if (curCell === undefined) break;
+    area++;
+    adjacentCells.forEach(({ x, y }) => {
+      const curX = curCell.coordinates.x + x;
+      const curY = curCell.coordinates.y + y;
+      if (curX < 0 || curX >= grid.length) return;
+      if (curY < 0 || curY >= grid[0].length) return;
+      const nextCell = grid[curX][curY];
+      if (nextCell.pentomino.name !== PENTOMINOES.None.name) return;
+      nextCell.pentomino = PENTOMINOES.Found;
+      tileQueue.push(nextCell);
     });
-  });
-  return valid.reduce((acc, val) => {
-    return acc && val.result;
-  }, true);
-}
-
-export function cornerIsValidSquare(grid: PlacedPentomino[][], startingCoords: Coordinates) {
-  let valid = false;
-  const { x, y } = startingCoords;
-  range(2).forEach((i) => {
-    const cell1 = grid[getWrap(x, 2, grid, "x")][getWrap(y, i, grid, "y")];
-    if (cell1.pentomino.name === PENTOMINOES.None.name) valid = true;
-    const cell2 = grid[getWrap(x, i, grid, "x")][getWrap(y, 2, grid, "y")];
-    if (cell2.pentomino.name === PENTOMINOES.None.name) valid = true;
-  });
-  return valid;
+  }
+  return area;
 }
